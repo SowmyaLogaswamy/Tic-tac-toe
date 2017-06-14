5e9e18ddeb983b8b76f3f6fd38a463c52719eaf1
Sowmya Logaswamy and Steve Zaske <steve@>
2017-06-14 14:42:56 -0700

    Add some more elements in the user interface and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 //Business logic
-var whoseTurn = true; //true= x's turn
+var xTurn = true; //true= x's turn
 var boardArray = [0,0,0,0,0,0,0,0,0]; //This helps us to track the status of each position.
+var stripes =["stripe1","stripe2","stripe3","stripe4","stripe5","stripe6","stripe7","stripe8"];
+//combination of winning stripes
+var winners = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+//Object constructor for GameState
+function GameState(winner,winningStripe) {
+  this.winner = winner;
+  this.winningStripe = winningStripe;
+}
+
+
 
 // checks to see if the position is clear
 function positionIsClear(pos) {
@@ -10,20 +21,93 @@ function positionIsClear(pos) {
   }
   return false;
 }
+
+function boardStatus(){
+  for (var stripeCount = 0; stripeCount < winners.length; stripeCount++){
+    var winnerCheck = "";
+    var winner = "none";
+
+    //are there 3 x's or o's in a row?
+    for (var stripe = 0; stripe < winners[stripeCount].length; stripe++) {
+      winnerCheck = winnerCheck + boardArray[winners[stripeCount][stripe]];
+    }
+
+    if (winnerCheck === "XXX"){
+      winner = "X";
+      break;
+    } else if (winnerCheck === "OOO") {
+      winner = "O";
+      break;
+    }
+  }
+  var tempState = new GameState(winner, stripeCount);
+ return tempState;
+}
+
+
+
+/////////////////////////////////////////////////////////////
 //User Interface Logic
+function highlightWin(stripe) {
+  var positionsArray=winners[stripe];
+  positionsArray.forEach(function(pos){
+    $(".position[data-pos='"+pos+"']").addClass("winner");
+  });
+}
+
+
 $(document).ready(function() {
 
 // an event listener that fires on any click on the board
 $(".position").on("click",function() {
-  var posClicked = $(this).data("pos") - 1;
-  if (positionIsClear(posClicked)) {
-    boardArray[posClicked]++}
+  var posClicked = $(this).data("pos");
+
+  if (positionIsClear(posClicked) && boardStatus().winner==='none') {
+    // check whose turn it is
+    if (xTurn) {
+      //make the clicked box X
+      boardArray[posClicked] = "X";
+    }
+    else {
+      //make the clicked box O
+      boardArray[posClicked] = "O";
+    }
+
+// code here to see if someone won
+    var status = boardStatus();
+
+    //if there is a winner do this
+    if (status.winner != "none"){
+      //highlights the winning row
+      highlightWin(status.winningStripe);
+
+      //stop all future clicks
+
+
+    }
+    else {
+
+    }
+
+
+    // increment the whoseTurn var (at end of the turn)
+    xTurn = !xTurn;
+
+    /////////////////////////////
+    // Auto update the full HTML content  based on boardArray
+    for (var index = 0;index <= boardArray.length;index++) {      //update each position
+      $("#pos"+(index+1)).text(boardArray[index]);
+    }
+
+    // This section is when there is an error state, they've clicked on a full position
+  } else {
+    //alert the user that the space is not clear
+    console.log("Place is already occupied");
+  }
 
 
   //console.log($(this).data("pos")+" was clicked");
-  console.log(boardArray);
+  console.log(boardArray, " turn is:", xTurn);
 })
 
 });
-
-//test
